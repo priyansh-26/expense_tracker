f61bd1ef1e7b90673a2559e79f0631cf4ca5fbb2
Priyansh khanna <145784273+priyansh-26@users.noreply.github.com>
2025-04-11 20:43:30 +0530

    expense_work

---
+++ lib/main.dart
@@ -0,0 +1,31 @@
+import 'package:device_preview/device_preview.dart';
+import 'package:expense_tracker/pages/homepage.dart';
+import 'package:expense_tracker/theme.dart';
+import 'package:flutter/cupertino.dart';
+import 'package:flutter/material.dart';
+import 'package:hive/hive.dart';
+import 'package:hive_flutter/hive_flutter.dart';
+import 'package:shared_preferences/shared_preferences.dart';
+
+late SharedPreferences preferences;
+void main() async {
+  await Hive.initFlutter();
+  await Hive.openBox('money');
+  preferences = await SharedPreferences.getInstance();
+  runApp(DevicePreview(builder: (context) => MyApp()));
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({Key? key}) : super(key: key);
+
+  // This widget is the root of your application.
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      debugShowCheckedModeBanner: false,
+      title: 'Expenses',
+      theme: myTheme,
+      home: const HomePage(),
+    );
+  }
+}
